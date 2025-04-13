85d20aaa41e2a6e2ec943a1eb25221f2ae96c272
SoumitraRai <raisoumitra123@gmail.com>
2025-04-13 21:56:43 +0530

    Initial commit

---
+++ backend/test.js
@@ -0,0 +1,11 @@
+const { PrismaClient } = require('./generated/prisma');
+const prisma = new PrismaClient();
+
+async function main() {
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
+
+main()
+  .catch(e => console.error(e))
+  .finally(() => prisma.$disconnect());
